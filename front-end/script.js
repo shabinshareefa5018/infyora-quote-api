61e7bb154f60af07d04d97e27be33b8cb899830e
Shabin Shareefa <shabinshareefa5018@gmail.com>
2025-12-04 14:40:30 +0530

    Update quote API endpoint in getQuote function

--- front-end/script.js
+++ front-end/script.js
@@ -1,6 +1,6 @@
 async function getQuote() {
     try {
-        const response = await fetch("https://infyora-quote-api-azeemroshan111-dev.apps.rm1.0a51.p1.openshiftapps.com//quote");
+        const response = await fetch("https://quote-api-shabinshareefa5018-dev.apps.rm2.thpm.p1.openshiftapps.com//quote");
         const data = await response.json();
         document.getElementById("quoteText").innerText = data.quote;
     } catch (error) {
