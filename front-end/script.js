1cfc24c123bc5cc2bc3eff8fb210a424a999f383
azeemroshanp-cyber <azeemroshanp@gmail.com>
2025-12-04 12:41:55 +0530

    Update quote API URL in getQuote function

--- front-end/script.js
+++ front-end/script.js
@@ -1,15 +1,6 @@
 async function getQuote() {
     try {
-        const response = await fetch("async function getQuote() {
-    try {
-        const response = await fetch("https://<ROUTE_URL>/quote");
-        const data = await response.json();
-        document.getElementById("quoteText").innerText = data.quote;
-    } catch (error) {
-        document.getElementById("quoteText").innerText = "Error fetching quote.";
-    }
-}
-/quote");
+        const response = await fetch("https://infyora-quote-api-azeemroshan111-dev.apps.rm1.0a51.p1.openshiftapps.com//quote");
         const data = await response.json();
         document.getElementById("quoteText").innerText = data.quote;
     } catch (error) {
